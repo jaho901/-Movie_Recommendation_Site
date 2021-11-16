3c40771fb0a8d0db8992fa3225f92f3eeb8afe90
정재호 <jaho901@naver.com>
2021-11-17 02:33:58 +0900

    movie_db done

--- back-end/movies/models.py
+++ back-end/movies/models.py
@@ -1,3 +1,25 @@
+from io import open_code
 from django.db import models
+# from django.db import settings
 
 # Create your models here.
+class Movie(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=50)
+    vote = models.FloatField()
+    country = models.CharField(max_length=50)
+    time = models.CharField(max_length=10)
+    open_date = models.CharField(max_length=10)
+    grade = models.CharField(max_length=10)
+    poster_path = models.TextField()
+    genre_1 = models.CharField(max_length=10)
+    genre_2 = models.CharField(max_length=10, default='')
+    genre_3 = models.CharField(max_length=10, default='')
+    genre_4 = models.CharField(max_length=10, default='')
+    actor_1 = models.CharField(max_length=20)
+    actor_2 = models.CharField(max_length=20, default='')
+    actor_3 = models.CharField(max_length=20, default='')
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
