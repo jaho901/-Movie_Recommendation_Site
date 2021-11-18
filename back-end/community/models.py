e72ef73f2fe7ed93f64b6ed40c617294fc5fd758
정재호 <jaho901@naver.com>
2021-11-18 20:37:07 +0900

    community start

--- back-end/community/models.py
+++ back-end/community/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+from movies.models import Movie
 
 # Create your models here.
+
+class Community(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_community')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='community')
+    community_title = models.CharField(max_length=50)
+    movie_title = models.CharField(max_length=50)
+    content = models.TextField()
--- back-end/community/views.py
+++ back-end/community/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import serializers, status
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Community
+from .serializers import CommunitySerializer
+
+@api_view(['GET'])
+def community_list(request):
+    community_li = Community.objects.all()
+    serializer = CommunitySerializer(community_li, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def movie_review_create(request, movie_id):
+    community = get_object_or_404(Community, pk=movie_id)
+    serializer = CommunitySerializer(data=request.data)
+
+    if serializer.is_valid(raise_exception=True):
+        serializer.community = community
+        serializer.user = request.user
+        serializer.save()
+        return Response(serializer.data)
+
