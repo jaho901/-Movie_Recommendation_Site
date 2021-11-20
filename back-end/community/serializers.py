0c8e1d2f25404b01b2d2b2a12e25a08545a0e910
dongjun <qkrehdwns96@naver.com>
2021-11-20 18:02:46 +0900

    user_done

--- back-end/community/serializers.py
+++ back-end/community/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Community
+from accounts.serializers import UserSerializer
 
 
 class CommunitySerializer(serializers.ModelSerializer):
-    
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Community
         fields = '__all__'
