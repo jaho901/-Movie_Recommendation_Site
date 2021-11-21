65c86ebe2cc083c04d3bfebab7fb103b1222f8d6
정재호 <jaho901@naver.com>
2021-11-22 00:09:37 +0900

    go

--- back-end/recommendation/urls.py
+++ back-end/recommendation/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 
 app_name="recommendation"
 
-router = DefaultRouter()
-router.register('', views.MovieListViewSet)
+# router = DefaultRouter()
+# router.register('', views.MovieListViewSet)
 
-urlpatterns = [
-    path('recommend_by_day_of_week/', views.MovieRecommendByDayViewSet.as_view({'get': 'list'})),
-]
+# urlpatterns = [
+#     path('recommend_by_day_of_week/', views.MovieRecommendByDayViewSet.as_view({'get': 'list'})),
+# ]
